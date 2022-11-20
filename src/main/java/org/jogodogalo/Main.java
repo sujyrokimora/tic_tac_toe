f42ca799b82215acbef60df1a6504c7fa9c2b665
sujyrokimora <cruz.xavier8@gmail.com>
2022-11-20 21:00:28 +0000

    First Version

---
+++ src/main/java/org/jogodogalo/Main.java
@@ -0,0 +1,21 @@
+package org.jogodogalo;
+import java.util.*;
+
+/** Este template foi criado na
+ * Escola Superior de Tecnologia e Gestão do
+ * Instituto Politécnico de Beja
+ * em 2021/09/20
+ * -----------------------------------------------------
+ * Adicione aqui uma descrição da classe, o seu nome e a data
+ * @author Rodrigo,Xavier Cruz
+ * @version 0.1
+ *
+ * O programa deve ser escrito em inglês.
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
